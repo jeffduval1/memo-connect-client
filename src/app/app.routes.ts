f333287cdd83aceb04ea9a24ba9ee99ce9aba737
jeffduval1 <kfu1@hotmail.fr>
2025-08-21 14:07:32 -0400

    Progression Angular; exclu NUL réservé Windows

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+      path: '',
+      loadComponent: () =>
+        import('./features/home/pages/home/home')
+          .then(m => m.HomeComponent)
+    },
+    {
+        path: 'cards',
+        loadComponent: () =>
+          import('./features/cards/pages/cards-list/cards-list')
+            .then(m => m.CardsListComponent)
+      }
+  ];
